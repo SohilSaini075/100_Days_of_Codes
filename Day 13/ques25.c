9c67e4ba6709aeaaf295e89a29545a5a34800cdb
SohilSaini75 <sohilsaini075@gmail.com>
2025-09-03 18:27:23 +0530

    Day 13 Ques

---
+++ Day 13/ques25.c
@@ -0,0 +1,62 @@
+// Q25: Write a program to implement a basic calculator using switch-case for +, -, *, /, %.
+
+/*
+Sample Test Cases:
+Input 1:
+4 2 +
+Output 1:
+6
+
+Input 2:
+10 3 %
+Output 2:
+1
+
+Input 3:
+15 5 /
+Output 3:
+3
+
+*/
+
+#include <stdio.h>
+
+int main() {
+    int num1, num2;
+    char op;
+
+    printf("Enter first number: ");
+    scanf("%d", &num1);
+    printf("Enter second number: ");
+    scanf("%d", &num2);
+    printf("Enter operator (+, -, *, /, % ): ");
+    scanf(" %c", &op);
+
+    switch(op) {
+        case '+':
+            printf("Result: %d\n", num1 + num2);
+            break;
+        case '-':
+            printf("Result: %d\n", num1 - num2);
+            break;
+        case '*':
+            printf("Result: %d\n", num1 * num2);
+            break;
+        case '/':
+            if (num2 != 0)
+                printf("Result: %d\n", num1 / num2);
+            else
+                printf("Error: Division by zero not allowed.\n");
+            break;
+        case '%':
+            if (num2 != 0)
+                printf("Result: %d\n", num1 % num2);
+            else
+                printf("Error: Modulo by zero not allowed.\n");
+            break;
+        default:
+            printf("Invalid operator!\n");
+    }
+
+    return 0;
+}
