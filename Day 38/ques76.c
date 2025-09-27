4dad1f8a5313c8558e8fd16780ac1783ef045eda
SohilSaini75 <sohilsaini075@gmail.com>
2025-09-27 10:47:47 +0530

    Day 38 Ques

---
+++ Day 38/ques76.c
@@ -0,0 +1,38 @@
+//Q76: Check if a matrix is symmetric.
+
+#include <stdio.h>
+
+int main() {
+    int n, m;
+    scanf("%d %d", &n, &m);
+
+    int a[n][m];
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    if (n != m) {
+        printf("False\n");
+        return 0;
+    }
+
+    int isSymmetric = 1;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (a[i][j] != a[j][i]) {
+                isSymmetric = 0;
+                break;
+            }
+        }
+        if (!isSymmetric) break;
+    }
+
+    if (isSymmetric)
+        printf("True\n");
+    else
+        printf("False\n");
+
+    return 0;
+}
