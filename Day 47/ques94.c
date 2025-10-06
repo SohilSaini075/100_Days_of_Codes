50beb000d10b335f2e0ce40b17d2c8a8c9aeedac
SohilSaini75 <sohilsaini075@gmail.com>
2025-10-06 09:27:03 +0530

    Day 47 Ques

---
+++ Day 47/ques94.c
@@ -0,0 +1,43 @@
+//Q94: Find the longest word in a sentence.
+
+/*
+Sample Test Cases:
+Input 1:
+I love programming
+Output 1:
+programming
+
+*/
+
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str[200];
+    char word[50], longest[50];
+    int i = 0, j = 0, maxLen = 0, len = 0;
+
+    fgets(str, sizeof(str), stdin);
+    str[strcspn(str, "\n")] = '\0';
+
+    while (1) {
+        if (str[i] == ' ' || str[i] == '\0') {
+            word[j] = '\0';
+            len = strlen(word);
+            if (len > maxLen) {
+                maxLen = len;
+                strcpy(longest, word);
+            }
+            j = 0;
+            if (str[i] == '\0')
+                break;
+        } else {
+            word[j++] = str[i];
+        }
+        i++;
+    }
+
+    printf("%s\n", longest);
+
+    return 0;
+}
