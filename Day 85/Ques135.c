e659e0e2a777d529f284e824b7f5423dde47677b
SohilSaini75 <sohilsaini075@gmail.com>
2025-11-13 16:40:47 +0530

    Day 85 Ques

---
+++ Day 85/Ques135.c
@@ -0,0 +1,30 @@
+//Q135: Print the initials of a name.
+
+/*
+Sample Test Cases:
+Input 1:
+John Doe
+Output 1:
+J.D.
+
+*/
+
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char name[100];
+    fgets(name, sizeof(name), stdin);
+
+    name[strcspn(name, "\n")] = '\0';
+
+    if (name[0] != ' ')
+        printf("%c.", name[0]);
+
+    for (int i = 0; name[i] != '\0'; i++) {
+        if (name[i] == ' ' && name[i + 1] != '\0' && name[i + 1] != ' ')
+            printf("%c.", name[i + 1]);
+    }
+
+    return 0;
+}
