a54e0eb971849b31f57f4ea6a52c6ae2e879a12e
SohilSaini75 <sohilsaini075@gmail.com>
2025-11-22 00:19:39 +0530

    Day 94 Ques

---
+++ Day 94/Ques144.c
@@ -0,0 +1,30 @@
+//Q144: Write a function that accepts a structure as parameter and prints its members.
+
+/*
+Sample Test Cases:
+Input 1:
+Student: Neha 104 92
+Output 1:
+Name: Neha | Roll: 104 | Marks: 92
+
+*/
+
+#include <stdio.h>
+#include <string.h>
+#define MAX_NAME_LENGTH 50
+struct Student {
+    char name[MAX_NAME_LENGTH];
+
+    int roll_no;
+    int marks;
+};
+void print_student(struct Student student) {
+    printf("Name: %s | Roll: %d | Marks: %d\n", student.name, student.roll_no, student.marks);
+}
+int main() {
+    struct Student student;
+    printf("Enter name, roll number and marks of the student: ");
+    scanf("%s %d %d", student.name, &student.roll_no, &student.marks);
+    print_student(student);
+    return 0;
+}
